545de635f58745381bf061447a0aef53cc579e03
nadrianfikri <ahmad.alfikri.n91@gmail.com>
2021-11-22 00:11:19 +0700

    rest api is done

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -1,10 +1,13 @@
 // import model
 const { Task } = require('../../models');
 
+// create
 exports.addTask = async (req, res) => {
   try {
-    const data = req.body;
-    await Task.create(data);
+    const data = await Task.create({
+      ...req.body,
+      status: 'active',
+    });
 
     res.send({
       status: 'success',
@@ -16,6 +19,123 @@ exports.addTask = async (req, res) => {
     res.status(500).send({
       status: 'Failed',
       message: 'Server error',
+    });
+  }
+};
+
+// read
+exports.getAllTask = async (req, res) => {
+  try {
+    const allTaskData = await Task.findAll({
+      attributes: {
+        exclude: ['createdAt', 'updatedAt'],
+      },
+      order: [['createdAt', 'ASC']],
+    });
+    res.send({
+      status: 'success',
+      message: 'Get all data success',
+      data: allTaskData,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      status: 'Failed',
+      message: 'Server error',
+    });
+  }
+};
+
+// read one data
+exports.getTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const taskData = await Task.findOne({
+      where: {
+        id,
+      },
+    });
+
+    // if null data send error status
+    if (taskData.id === null) {
+      res.status(500).send({
+        status: 'Failed',
+        message: 'Server error',
+      });
+      return;
+    }
+
+    res.send({
+      status: 'success',
+      message: 'Get data success',
+      data: taskData,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      status: 'Failed',
+      message: 'Server error',
+    });
+  }
+};
+
+// update
+exports.updateTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Task.update(req.body, {
+      where: {
+        id,
+      },
+    });
+
+    const updatedData = await Task.findOne({
+      where: {
+        id,
+      },
+    });
+
+    // if null data send error status
+    if (updatedData.id === null) {
+      res.status(500).send({
+        status: 'Failed',
+        message: 'Server error',
+      });
+      return;
+    }
+
+    res.send({
+      status: 'success',
+      message: 'update task is successfull',
+      data: updatedData,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      status: 'Failed',
+      message: 'Server error',
+    });
+  }
+};
+
+// delete
+exports.deleteTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Task.destroy({
+      where: {
+        id,
+      },
+    });
+    res.send({
+      status: 'success',
+      message: `delete task with id: ${id} success`,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      status: 'failed',
+      message: 'server error',
     });
   }
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 // controller
-const { addTask } = require('../controllers/taskController');
+const { addTask, getAllTask, getTask, updateTask, deleteTask } = require('../controllers/taskController');
 
 // routes task
 router.post('/task', addTask);
+router.get('/task', getAllTask);
+router.get('/task/:id', getTask);
+router.patch('/task/:id', updateTask);
+router.delete('/task/:id', deleteTask);
 
 module.exports = router;
