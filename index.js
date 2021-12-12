2fa00f2a6e556eadadac0970bd925719f87af4b0
nadrianfikri <ahmad.alfikri.n91@gmail.com>
2021-12-12 23:42:22 +0700

    fix error app

--- index.js
+++ index.js
@@ -11,5 +11,6 @@ app.use(cors());
 
 // grouping router endpoint
 app.use('/api/v1/', router);
+app.use('/', 'Hello Todo Api');
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
